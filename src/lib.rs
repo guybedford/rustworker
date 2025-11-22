958601f887b70bb73c9c2917f354ba7a10947ec4
Guy Bedford <gbedford@cloudflare.com>
2025-11-21 18:39:44 -0800

    generics

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use js_sys::{JsString, Reflect};
+use js_sys::{JsString, Promise, Reflect};
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
 use web_sys::{Request, Response, Url};
 use worker::DurableObject;
 
@@ -33,7 +34,7 @@ extern "C" {
     pub type MyDurableObjectStub;
 
     #[wasm_bindgen(method, js_name = "sayHello")]
-    pub async fn say_hello(this: &MyDurableObjectStub) -> JsString;
+    pub fn say_hello(this: &MyDurableObjectStub) -> Promise<String>;
 }
 // in lieu of automated bindgen / generics (for now!)...
 #[wasm_bindgen]
@@ -54,7 +55,7 @@ pub async fn fetch(request: Request, env: JsValue) -> Result<Response, JsValue> 
     )?)
     .get_by_name(&Url::new(&request.url())?.pathname());
 
-    let greeting = stub.say_hello().await.as_string().unwrap();
+    let greeting = JsFuture::from(stub.say_hello()).await?.from_js();
 
     Response::new_with_opt_str(Some(&greeting))
 }
